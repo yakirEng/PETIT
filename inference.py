7c27d3b5b5be25dbf2d68fddbbffee9e729b5ca0
yakir_hadad <yakirhadad18@gmail.com>
2024-11-21 17:59:40 +0200

    created infernce file for petit

--- inference.py
+++ inference.py
@@ -13,44 +13,25 @@ from src.configs import TestConfig
 from src.dataset import MonoDS
 
 
-def main():
+def inference():
     config = TestConfig()
+    batch_size = 100
+    fmt_of_output = "png"
+
     model = initialize_petit_model(config)
 
-
     dataset = MonoDS(src_dir=Path("data", "pan", "test"))
+    dataloader = create_dataloader(config, dataset, batch_size)
 
-    dataloader = create_dataloader(config, dataset)
-
-    fmt_of_output = "png"
     path_to_save = Path.cwd() / 'results' / 'transformed' / datetime.now().strftime("%Y%m%d_h%Hm%Ms%S")
     path_to_save.mkdir(parents=True, exist_ok=True)
 
-    with torch.inference_mode():
-        for i, data in enumerate(tqdm(dataloader, desc="Test")):
-            model.set_input(data)
-            model.forward()
-
-            visuals = model.get_current_visuals()
-
-            for type in ["pan_real", "mono_fake", "mono_phys"]:
-                sub_dir = path_to_save / type
-                sub_dir.mkdir(parents=True, exist_ok=True)
-                cur_vis = visuals[type]
-                domain = type.split("_")[0]
-                images = model.rec_image(cur_vis, domain=domain, fmt=fmt_of_output)
-                if fmt_of_output == "npy":
-                    for j, image in enumerate(images):
-                        np.save(f"{str(sub_dir)}/{i * batch_size + j}.npy", image)
-                else:
-                    for j, image in enumerate(images):
-                        pil_img = Image.fromarray(image)
-                        pil_img.save(f"{str(sub_dir)}/{i * batch_size + j}.{fmt_of_output}")
+    run_model(model, dataloader, path_to_save, batch_size, fmt_of_output)
 
 
 
-def create_dataloader(config, dataset):
-    batch_size = 100
+def create_dataloader(config, dataset, batch_size=1):
+
     dataloader = torch.utils.data.DataLoader(
         dataset,
         batch_size=batch_size,
@@ -72,7 +53,30 @@ def initialize_petit_model(config):
     model.set_phase(NetPhase.test)
     model.setup(config)
     model.load_networks("best")
-
     return model
+
+def run_model(model, dataloader, path_to_save, batch_size=100, fmt_of_output="npz"):
+    with torch.inference_mode():
+        for i, data in enumerate(tqdm(dataloader, desc="Test")):
+            model.set_input(data)
+            model.forward()
+            visuals = model.get_current_visuals()
+
+            for type in ["pan_real", "mono_fake", "mono_phys"]:
+                sub_dir = path_to_save / type
+                sub_dir.mkdir(parents=True, exist_ok=True)
+                cur_vis = visuals[type]
+                domain = type.split("_")[0]
+                images = model.rec_image(cur_vis, domain=domain, fmt=fmt_of_output)
+                if fmt_of_output == "npy":
+                    for j, image in enumerate(images):
+                        np.save(f"{str(sub_dir)}/{i * batch_size + j}.npy", image)
+                else:
+                    for j, image in enumerate(images):
+                        pil_img = Image.fromarray(image)
+                        pil_img.save(f"{str(sub_dir)}/{i * batch_size + j}.{fmt_of_output}")
+
+
+
 
 
